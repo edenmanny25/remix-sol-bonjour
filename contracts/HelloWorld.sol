10e5ba7e3c42ef5138e7948c69522a48b54a8b2f
edenmany25 <manny.bernard88@gmail.com>
2024-02-05 15:34:07 -0500

    update add

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.12 <0.9.0;
 
 contract HelloWorld {
   /**
-   * @dev Prints Hello World string some alsoe try 
+   * @dev Prints Hello World string some alsoe try  add to it 
    */
   function print() public pure returns (string memory) {
     return "Hello World!!!!";
