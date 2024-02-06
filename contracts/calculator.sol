6c27815bbf4bfb81d19884ab14e6538266f7e214
edenmanny25 <manny.bernard88@gmail.com>
2024-02-06 02:42:14 +0000

    step 3

--- contracts/calculator.sol
+++ contracts/calculator.sol
@@ -4,12 +4,29 @@ pragma solidity >=0.6.12 <0.9.0;
 
 
 
-// 1️⃣ Make a contract called Calculator
-// 2️⃣ Create Result variable to store result
-// 3️⃣ Create functions to add, subtract, and multiply to result
+// 1️⃣ Make a contract called Calculator [done]
+// 2️⃣ Create Result variable to store result [done]
+// 3️⃣ Create functions to add, subtract, and multiply to result 
 // 4️⃣ Create a function to get result
 
 contract Calculator {
     uint256 public result;
+
+    unction add( uint256 num) external {
+        result += num;
+    }
+
+    function subtract(uint256 num) public {
+        result -= num;
+    }
+
+    function multiply(uint256 num) public {
+        result *= num;
+    }
+
+    function get() public view  returns (uint256) {
+        return result;
+    }
+
 
 }
