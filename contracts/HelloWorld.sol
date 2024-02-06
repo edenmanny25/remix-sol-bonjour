7a2ccb0b9f38bcebfdf89a5760fd4b7aba02c98f
edenmanny25 <manny.bernard88@gmail.com>
2024-02-06 01:52:10 +0000

    local dev

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.6.12 <0.9.0;
 
 contract HelloWorld {
   /**
-   * @dev Prints Hello World string some alsoe try  add to it 
+   * @dev Prints Hello World string some alsoe try  add to it local dev  
    */
   string public welcome = "this is a public string ";
   
